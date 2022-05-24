509209614c1511b68fff6a1bad462912fd038f92
martín etcheberry <msetcheberry@gmail.com>
2022-05-23 22:30:24 -0300

    Agrego el inicio del componente de Lista de Compras

--- src/components/AppListaDeCompras.vue
+++ src/components/AppListaDeCompras.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    <h1>Lista</h1>
+    <div class="display justify-content-center">
+      <div class="p-3 mb-4">
+        <div class="col text-center">
+          <router-link to="/agregar-productos" class="mx-5 boton"
+            >Agregar productos</router-link
+          >
+          <router-link to="/realizar-compra" class="mx-5 boton"
+            >Realizar compra</router-link
+          >
+          <h5 class="mx-5 w-25"><u>Lista</u></h5>
+        </div>
+      </div>
+    </div>
+    <h2 class="mx-5">Acá se llamaría al componente lista</h2>
+    <!-- <AppLista/> -->
   </div>
 </template>
 
@@ -9,4 +23,7 @@ export default {};
 </script>
 
 <style>
+.amarillo {
+  background-color: yellow;
+}
 </style>
