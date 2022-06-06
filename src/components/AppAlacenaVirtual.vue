63915b0082473863f655b660c07909ae3552e00b
Nicolas Zapata <ni.c.ozapa969696@hotmail.com>
2022-06-06 20:59:32 -0300

    Creamos el componente Alacena Virtual

--- src/components/AppAlacenaVirtual.vue
+++ src/components/AppAlacenaVirtual.vue
@@ -1,11 +1,44 @@
 <template>
-  <div>
+  <div class = "container">
     <h1>Alacena</h1>
+   
+  <div class="input-group">
+  <input type="text" class="form-control rounded" placeholder="Burcar Producto" aria-label="Search" aria-describedby="search-addon" />
+  <button type="button" class="btn btn-outline-primary">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+          </svg>
+  </button>
   </div>
+  <div class="m-3">
+      <AppLista :precio="2559" :nombre="nombreLista"></AppLista>
+</div>
+</div>
+
+
+  
 </template>
 
 <script>
-export default {};
+import AppLista from "@/components/AppLista.vue";
+import { useStore } from "../store/store.js";
+export default {
+  components: {
+    AppLista,
+  },
+  data() {
+    return {
+      nombreLista: "",
+    };
+  },
+  setup() {
+    const store = useStore();
+    return { store };
+  },
+  created() {
+    this.nombreLista = this.store.listaDeCompras.name;
+  },
+};
 </script>
 
 <style>
